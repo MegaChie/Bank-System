1b07ff142f60bc65133deaac0fa4936b2dc27c0d
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-12-22 17:53:01 +0000

    Add Program file for the API

--- Bank-System/Program.cs
+++ Bank-System/Program.cs
@@ -1,25 +1,22 @@
+using Bank_System.Data;
+using Bank_System.Repositories;
+using Bank_System.Repositories.Interfaces;
+using Bank_System.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BankingContext>(options =>
+    options.UseSqlite(builder.Configuration.
+                      GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<AccountService>();
+builder.Services.AddScoped<TransactionService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
