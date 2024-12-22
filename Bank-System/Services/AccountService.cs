ae18b50187406bde518e341a526dc39ce05273b0
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-12-22 13:38:31 +0000

    Add Services folder for API

---
+++ Bank-System/Services/AccountService.cs
@@ -0,0 +1,94 @@
+using System.Threading.Tasks;
+using System.Transactions;
+using Bank_System.Models;
+using Bank_System.Repositories.Interfaces;
+
+namespace Bank_System.Services
+{
+    public class AccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ITransactionRepository _transactionRepository;
+
+        public AccountService(IAccountRepository accountRepository, ITransactionRepository transactionRepository)
+        {
+            _accountRepository = accountRepository;
+            _transactionRepository = transactionRepository;
+        }
+
+        public async Task DepositAsync(int accountID, decimal amount)
+        {
+            var account = await _accountRepository.GetAccountByIdAsync(accountID);
+            if (account == null)
+            {
+                throw new KeyNotFoundException("Dis account aint there");
+            }
+            account.Balance += amount;
+            await _accountRepository.UpdateAccountAsync(account);
+
+            var transaction = new Transaction
+            {
+                AccountID = accountID,
+                Amount = amount,
+                TransactionType = "Deposit",
+                Timestamp = DateTime.UtcNow
+            };
+            await _transactionRepository.AddTransactionAsync(transaction);
+        }
+
+        public async Task WithdrawAsync(int accountID, decimal amount)
+        {
+            var account = await _accountRepository.GetAccountByIdAsync(accountID);
+            if (account == null)
+            {
+                throw new KeyNotFoundException("Dis account aint there");
+            }
+            if (account is CheckingAccount checkingAccount)
+            {
+                if (account.Balance - amount < -checkingAccount.Overdraft)
+                {
+                    throw new InvalidOperationException("Overdraft limit exceeded");
+                }
+            }
+            else if (account is SavingsAccount && account.Balance - amount < 0)
+            {
+                throw new InvalidOperationException("You do not have this much");
+            }
+            account.Balance -= amount;
+            await _accountRepository.UpdateAccountAsync(account);
+
+            var transaction = new Transaction
+            {
+                AccountID = accountID,
+                Amount = -amount,
+                TransactionType = "Withdrawal",
+                Timestamp = DateTime.UtcNow
+            };
+            await _transactionRepository.AddTransactionAsync(transaction);
+        }
+
+        public async Task TransferAsync(int fromAccountID, int toAccountID, decimal amount)
+        {
+            var fromAccount = await _accountRepository.GetAccountByIdAsync(fromAccountID);
+            var toAccount = await _accountRepository.GetAccountByIdAsync(toAccountID);
+            if (fromAccount == null || toAccount == null)
+            {
+                throw new KeyNotFoundException("One or both account is not found!");
+            }
+
+            await WithdrawAsync(fromAccountID, amount);
+            await DepositAsync(toAccountID, amount);
+
+        }
+
+        public async Task<decimal> GetBalanceAsync(int accountID)
+        {
+            var account = await _accountRepository.GetAccountByIdAsync(accountID);
+            if (account == null)
+            {
+                throw new KeyNotFoundException("Dis account aint there");
+            }
+            return account.Balance;
+        }
+    }
+}
---
+++ Bank-System/Services/TransactionService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bank_System.Models;
+using Bank_System.Repositories.Interfaces;
+
+namespace Bank_System.Services
+{
+    public class TransactionService
+    {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionService(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactionsByAccountIdAsync(int accountID)
+        {
+            return await _transactionRepository.GetTransactionsByAccountIdAsync(accountID);
+        }
+    }
+}
